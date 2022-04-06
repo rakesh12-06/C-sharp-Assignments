4a0264069e6d965783761644647592591a25fb90
Rakesh T <102318948+rakesh12-06@users.noreply.github.com>
2022-04-06 09:47:28 +0530

    Assignment-1 (4)

---
+++ SwappingProgram.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swapping
+{
+    internal class Program
+    {
+        public static void SwapMethod(int num1,int num2)
+        {
+            num1= num1 + num2;
+            num2= num1 - num2;
+            num1= num1 - num2;
+            Console.WriteLine("Num1 = " + num1 + " Num2 = " + num2 );
+        }
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.WriteLine("Enter two Numbers");
+            num1=int.Parse(Console.ReadLine());
+            num2=int.Parse(Console.ReadLine());
+            Console.WriteLine("Before Swapping....");
+            Console.WriteLine("Num1 = " + num1 + " Num2 = " + num2);
+            Console.WriteLine("After Swapping ...");
+            SwapMethod(num1,num2);
+        }
+    }
+}
