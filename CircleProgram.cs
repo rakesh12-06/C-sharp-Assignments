8b61ac5850bf4cb1e596b251e03cff0ffcf3f1e9
Rakesh T <102318948+rakesh12-06@users.noreply.github.com>
2022-04-06 09:55:58 +0530

    Added Assignment-1 (5)

---
+++ CircleProgram.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    internal class Program
+    {
+        public static void CalculateAC(float r,float Pi)
+        {
+            float area, circum;
+            area = Pi*r*r;
+            circum = 2 * Pi * r;
+            Console.WriteLine("Area of Circle is : " + area);
+            Console.WriteLine("Circumference of Circle is : " + circum);
+        }
+        static void Main(string[] args)
+        {
+            const float Pi = 3.1428F;
+            float r;
+            Console.WriteLine("Enter radius of circle");
+            r = float.Parse(Console.ReadLine());
+            CalculateAC(r,Pi);
+        }
+    }
+}
