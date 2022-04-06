83ec6afc6caab50c951ece2e0bfdd8317f3af06d
Rakesh T <102318948+rakesh12-06@users.noreply.github.com>
2022-04-06 09:40:27 +0530

    Assignment-1 (1)

---
+++ CalculatorProgram.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1, num2;
+            char operator1; 
+            float answer;
+            Console.WriteLine("Enter first number");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter operator");
+            operator1 = char.Parse(Console.ReadLine());
+
+            switch (operator1)
+            {
+                case '+' :
+                    answer = num1 + num2;
+                    Console.WriteLine(answer);
+                    break;
+
+                case '-' :
+                    answer = num1 - num2;
+                    Console.WriteLine(answer);
+                    break;
+
+                case '/' :
+                    answer = num1 / num2;
+                    Console.WriteLine(answer);
+                    break;
+                
+                case '*':
+                    answer = num1 * num2;
+                    Console.WriteLine(answer);
+                    break;
+                
+                default:
+                    Console.WriteLine("Invalid operator ");
+                    break;
+            }
+
+        }
+    }
+}
