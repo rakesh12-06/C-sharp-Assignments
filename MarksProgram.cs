8f94ddc86be1fde6256d91f8153d8fd0972fd0b3
Rakesh T <102318948+rakesh12-06@users.noreply.github.com>
2022-04-06 09:43:26 +0530

    Added Assignment-1 (2)

---
+++ MarksProgram.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] arr = new float[5];
+            Console.WriteLine("Enter marks of five students");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = float.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Average Marks of 5 students is : " + arr.Average());
+            Console.WriteLine("Highest Marks obtaines is : " + arr.Max());
+        }
+    }
+}
