34cdc36c51ad7926949adbd4f6ff1708d4638269
Rakesh T <102318948+rakesh12-06@users.noreply.github.com>
2022-04-11 07:11:18 +0530

    Add files via upload

---
+++ Assignment 5/ArrayList2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionFramework
+{
+    public class Employee
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public double Salary { get; set; }  
+    }
+
+
+    internal class ArrayList2
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.Id = 1;
+            emp.Name = "Rakesh";
+            emp.Salary = 50000;
+            ArrayList empList = new ArrayList();
+            empList.Add(emp);
+            foreach (var data in empList)
+                Console.Write("Employee Id = {0}, Employee Name = {1}, Employee Salary =  {2} ", emp.Id, emp.Name , emp.Salary );
+        }
+    }
+}
+
+
