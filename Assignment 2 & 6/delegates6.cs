8447032f7e66e3ac5a606944a547085aa53ee2f1
Rakesh T <102318948+rakesh12-06@users.noreply.github.com>
2022-04-11 07:04:49 +0530

    Add files via upload

---
+++ Assignment 2 & 6/delegates6.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmployeeManagementSystem;
+
+namespace DelegateEmployee
+{
+    public delegate void EmployeeDelegate();
+    internal class delegates6
+    {
+        static void Main(string[] args)
+        {
+            Employee emp;
+            emp = new Manager("Arjun", 45000);
+            emp.CalculateSalary();
+            EmployeeDelegate employeeDelegate = new EmployeeDelegate(emp.PrintEmpDetails);
+            employeeDelegate();
+            emp = new MarketingExecutive("John", 65000, 25);
+            emp.CalculateSalary();
+            EmployeeDelegate employeeDelegate2 = new EmployeeDelegate(emp.PrintEmpDetails);
+            employeeDelegate2();
+        }
+    }
+}
