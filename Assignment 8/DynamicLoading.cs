95730676f91c83aa940368a34d54a3e49cf67658
Rakesh T <102318948+rakesh12-06@users.noreply.github.com>
2022-04-18 21:32:09 +0530

    Create DynamicLoading.cs

---
+++ Assignment 8/DynamicLoading.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace dynamic_loading
+{
+    public class hello_world
+    {
+        public string SayHello(string name) {
+            return "hello " + name;
+        }
+
+        public string SayBye(string name)
+        {
+            return "Bye " + name;
+        }
+    }
+}
